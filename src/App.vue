b7a8140415296749dd4ebe32937aa15a2e959c2e
Harlock221B <64704484+Harlock221B@users.noreply.github.com>
2024-07-28 13:44:38 -0300

    Instalando biblioteca de animações e componentizando a navbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title>Movie App</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn to="/">Home</v-btn>
-      <v-btn to="/favorites">Favorites</v-btn>
-    </v-app-bar>
+    <NavBar />
     <v-main>
       <router-view/>
     </v-main>
@@ -14,14 +9,12 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import NavBar from './components/NavBar.vue';
 
 export default defineComponent({
   name: 'App',
+  components: {
+    NavBar,
+  },
 });
 </script>
-
-<style>
-  .v-application {
-    font-family: 'Roboto', sans-serif;
-  }
-</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title class="animate__animated animate__fadeInLeft">MovieApp</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn to="/" class="animate__animated animate__fadeIn">Home</v-btn>
+      <v-btn to="/favorites" class="animate__animated animate__fadeIn">Favorites</v-btn>
+    </v-app-bar>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+  import { useRouter } from 'vue-router';
+  
+  export default defineComponent({
+    name: 'NavBar',
+  });
+  </script>
+  
+  <style scoped>
+  .v-toolbar-title {
+    font-family: 'Roboto', sans-serif;
+    font-size: 24px;
+    font-weight: bold;
+  }
+  
+  .v-btn {
+    font-family: 'Roboto', sans-serif;
+    font-size: 16px;
+    margin-right: 15px;
+    color: white;
+  }
+  
+  .animate__animated {
+    animation-duration: 1s;
+  }
+  
+  .animate__fadeInLeft {
+    --animate-duration: 1s;
+  }
+  
+  .animate__fadeIn {
+    --animate-duration: 1s;
+  }
+  </style>
+  
